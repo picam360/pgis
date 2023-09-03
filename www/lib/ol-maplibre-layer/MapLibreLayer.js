952fa7e72692e625b3b5d394551e669fedad992c
Takuma Hirayama <hirayama.takuma@ohmydigifab.com>
2023-09-03 22:11:40 +0900

    maplibre

---
+++ www/lib/ol-maplibre-layer/MapLibreLayer.js
@@ -0,0 +1,91 @@
+/**
+ * @typedef {Object} Options
+ * @property {Object<string, *>} maplibreOptions
+ */
+
+class MapLibreLayer extends ol.layer.Layer {
+  /**
+   * @param {Options} options
+   */
+  constructor(options) {
+    const baseOptions = Object.assign({}, options);
+
+    delete baseOptions.maplibreOptions;
+
+    super(baseOptions);
+
+    const container = document.createElement('div');
+    container.style.position = 'absolute';
+    container.style.width = '100%';
+    container.style.height = '100%';
+
+    this.maplibreMap = new maplibregl.Map(
+      Object.assign({}, options.maplibreOptions, {
+        container: container,
+        attributionControl: false,
+        interactive: false,
+        trackResize: false,
+      })
+    );
+
+    this.applyOpacity_();
+  }
+
+  /**
+   * @param {number} opacity
+   */
+  setOpacity(opacity) {
+    super.setOpacity(opacity);
+    this.applyOpacity_();
+  }
+
+  applyOpacity_() {
+    const canvas = this.maplibreMap.getCanvas();
+    const opacity = this.getOpacity().toString();
+    if (opacity !== canvas.style.opacity) {
+      canvas.style.opacity = opacity;
+    }
+  }
+
+  /**
+   * @param {import('ol/Map').FrameState} frameState
+   * @return {HTMLCanvasElement} canvas
+   */
+  render(frameState) {
+    const viewState = frameState.viewState;
+
+    // adjust view parameters in maplibre
+    this.maplibreMap.jumpTo({
+      center: ol.proj.toLonLat(viewState.center),
+      zoom: viewState.zoom - 1,
+      bearing: ol.math.toDegrees(-viewState.rotation),
+      animate: false,
+    });
+
+    const maplibreCanvas = this.maplibreMap.getCanvas();
+    if (!maplibreCanvas.isConnected) {
+      // The canvas is not connected to the DOM, request a map rendering at the next animation frame
+      // to set the canvas size.
+      this.getMapInternal().render();
+    } else if (!sameSize(maplibreCanvas, frameState)) {
+      this.maplibreMap.resize();
+    }
+
+    this.maplibreMap.redraw();
+
+    return this.maplibreMap.getContainer();
+  }
+}
+
+/**
+ *
+ * @param {HTMLCanvasElement} canvas
+ * @param {import('ol/Map').FrameState} frameState
+ * @return boolean
+ */
+function sameSize(canvas, frameState) {
+  return (
+    canvas.width === Math.floor(frameState.size[0] * frameState.pixelRatio) &&
+    canvas.height === Math.floor(frameState.size[1] * frameState.pixelRatio)
+  );
+}
